0acee6d32e4cfa911a9379f074556190c7659f57
Tomoya Kobayashi <tomocha.marika@gmail.com>
2022-06-05 10:12:15 +0900

    succeeded to more console log

--- src/lib.rs
+++ src/lib.rs
@@ -11,8 +11,14 @@ pub fn run() {
 extern "C" {
     #[wasm_bindgen(js_namespace = console)]
     fn log(s: &str);
+    #[wasm_bindgen(js_namespace = console, js_name = log)]
+    fn log_u32(a: u32);
+    #[wasm_bindgen(js_namespace = console, js_name = log)]
+    fn log_many(a: &str, b: &str);
 }
 
 fn bare_bones() {
     log("Hello fron rust");
+    log_u32(42);
+    log_many("Logging", "many values");
 }
